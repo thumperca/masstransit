490f9a3b8d5158fb30b4c57f2e9ceed544b05e2d
Manvir Singh <mail@manvir.me>
2024-01-03 13:38:03 +0530

    Added benchmark with std::channel

---
+++ src/bin/bench.rs
@@ -0,0 +1,168 @@
+use mass_transit::channel;
+use std::sync::mpsc::channel as std_channel;
+use std::thread;
+use std::time::Instant;
+
+fn generate_data(id: u8) -> Vec<String> {
+    (0..100_000).map(|item| format!("{id}-{item}")).collect()
+    // (0..5).map(|item| format!("{id}-{item}")).collect()
+}
+
+fn main() {
+    run_std();
+    run_custom_one();
+    run_custom_exact(100);
+    run_custom_all();
+}
+
+fn run_std() {
+    let (tx, rx) = std_channel();
+    let time = Instant::now();
+    thread::scope(|scope| {
+        let tx2 = tx.clone();
+        let tx3 = tx.clone();
+        let tx4 = tx.clone();
+        scope.spawn(move || {
+            for item in generate_data(1) {
+                tx.send(item);
+            }
+        });
+        scope.spawn(move || {
+            for item in generate_data(2) {
+                tx2.send(item);
+            }
+        });
+        scope.spawn(move || {
+            for item in generate_data(2) {
+                tx3.send(item);
+            }
+        });
+        scope.spawn(move || {
+            for item in generate_data(2) {
+                tx4.send(item);
+            }
+        });
+        scope.spawn(move || {
+            let mut counter = 0;
+            while let Ok(msg) = rx.recv() {
+                counter += 1;
+            }
+        });
+    });
+    let took = time.elapsed();
+    println!("Standard took {:?}", took);
+}
+
+fn run_custom_one() {
+    let (tx, rx) = channel();
+    let time = Instant::now();
+    thread::scope(|scope| {
+        let tx2 = tx.clone();
+        let tx3 = tx.clone();
+        let tx4 = tx.clone();
+        scope.spawn(move || {
+            for item in generate_data(1) {
+                tx.send(item);
+            }
+        });
+        scope.spawn(move || {
+            for item in generate_data(2) {
+                tx2.send(item);
+            }
+        });
+        scope.spawn(move || {
+            for item in generate_data(2) {
+                tx3.send(item);
+            }
+        });
+        scope.spawn(move || {
+            for item in generate_data(2) {
+                tx4.send(item);
+            }
+        });
+        scope.spawn(move || {
+            let mut counter = 0;
+            while let Some(msg) = rx.recv() {
+                counter += 1;
+            }
+        });
+    });
+    let took = time.elapsed();
+    println!("Custom recv took {:?}", took);
+}
+
+fn run_custom_exact(num: usize) {
+    let (tx, rx) = channel();
+    let time = Instant::now();
+    thread::scope(|scope| {
+        let tx2 = tx.clone();
+        let tx3 = tx.clone();
+        let tx4 = tx.clone();
+        scope.spawn(move || {
+            for item in generate_data(1) {
+                tx.send(item);
+            }
+        });
+        scope.spawn(move || {
+            for item in generate_data(2) {
+                tx2.send(item);
+            }
+        });
+        scope.spawn(move || {
+            for item in generate_data(2) {
+                tx3.send(item);
+            }
+        });
+        scope.spawn(move || {
+            for item in generate_data(2) {
+                tx4.send(item);
+            }
+        });
+        scope.spawn(move || {
+            let mut counter = 0;
+            while let Some(msg) = rx.recv_exact(num) {
+                counter += 1;
+            }
+        });
+    });
+    let took = time.elapsed();
+    println!("Custom recv_exact {:?}", took);
+}
+
+fn run_custom_all() {
+    let (tx, rx) = channel();
+    let time = Instant::now();
+    thread::scope(|scope| {
+        let tx2 = tx.clone();
+        let tx3 = tx.clone();
+        let tx4 = tx.clone();
+        scope.spawn(move || {
+            for item in generate_data(1) {
+                tx.send(item);
+            }
+        });
+        scope.spawn(move || {
+            for item in generate_data(2) {
+                tx2.send(item);
+            }
+        });
+        scope.spawn(move || {
+            for item in generate_data(2) {
+                tx3.send(item);
+            }
+        });
+        scope.spawn(move || {
+            for item in generate_data(2) {
+                tx4.send(item);
+            }
+        });
+        scope.spawn(move || {
+            let mut counter = 0;
+            while let Some(msg) = rx.recv_all() {
+                counter += 1;
+            }
+        });
+    });
+    let took = time.elapsed();
+    println!("Custom reacv_all {:?}", took);
+}
